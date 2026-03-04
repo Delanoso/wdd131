06932559fde8cc37811e9f94b3b2ae77178cdc2c
Delanoso <erichvandenheuvel5@gmail.com>
2026-03-04 08:50:49 +0200

    changes and testing of all Exercise 1 to 5

---
+++ week01/Exercise3/Program.cs
@@ -0,0 +1,56 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Guess My Number!");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string? input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    guess = int.Parse(input);
+                }
+                else
+                {
+                    guess = -1;
+                }
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
+    }
+}
